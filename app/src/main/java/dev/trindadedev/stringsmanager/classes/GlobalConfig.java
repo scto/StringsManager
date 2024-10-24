b11123dcf06a91c75bfdc7aa3dfd6f6772a0fe09
trindadedev13 <devsuay@gmail.com>
2024-10-24 01:28:43 -0300

    feat: better an

--- app/src/main/java/dev/trindadedev/stringsmanager/classes/GlobalConfig.java
+++ app/src/main/java/dev/trindadedev/stringsmanager/classes/GlobalConfig.java
@@ -4,8 +4,8 @@ import com.google.android.material.transition.MaterialSharedAxis;
 
 public class GlobalConfig {
     
-    public static final int SharedAxisExit = MaterialSharedAxis.X;
-    public static final int SharedAxisEnter = MaterialSharedAxis.X;
+    public static final int SharedAxisExit = MaterialSharedAxis.Y;
+    public static final int SharedAxisEnter = MaterialSharedAxis.Y;
     public static final boolean SharedAxisEnterBoolean = true;
     public static final boolean SharedAxisExitBoolean = false;
     
